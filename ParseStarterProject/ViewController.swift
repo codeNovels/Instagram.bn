ae090e93626f599edfa2b3bbc8cd0816682ff894
Billy Nguyen <codeNovels@gmail.com>
2017-01-14 14:38:44 -0600

    Active users table, login feature

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -14,9 +14,25 @@ class ViewController: UIViewController {
 	
 	var signupMode = true
 	
+	var activityIndicator = UIActivityIndicatorView()
+	
 	@IBOutlet weak var emailTextField: UITextField!
 	
 	@IBOutlet weak var passwordTextField: UITextField!
+	
+	func createAlert(title: String, message: String){
+		
+		let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+		
+		alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+			
+			self.dismiss(animated: true, completion: nil)
+			
+		}))
+		
+		self.present(alert, animated: true, completion: nil)
+		
+	}
 	
 	@IBOutlet weak var changeSignupModeButton: UIButton!
 	
@@ -28,15 +44,90 @@ class ViewController: UIViewController {
 		
 		if emailTextField.text == "" || passwordTextField.text == "" {
 			
-			let alert = UIAlertController(title: "Error in form", message: "Please enter a username and password", preferredStyle: UIAlertControllerStyle.alert)
+			createAlert(title: "Error in form", message: "Please enter a username and password")
 			
-			alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
-				
-				self.dismiss(animated: true, completion: nil)
-				
-			}))
 			
-				self.present(alert, animated: true, completion: nil)
+		} else {
+			
+			activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+			activityIndicator.center = self.view.center
+			activityIndicator.hidesWhenStopped = true
+			activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+			view.addSubview(activityIndicator)
+			activityIndicator.startAnimating()
+			UIApplication.shared.beginIgnoringInteractionEvents()
+			
+			if signupMode {
+				// sign up
+				
+				let user = PFUser()
+				
+				user.username = emailTextField.text
+				user.email = emailTextField.text
+				user.password = passwordTextField.text
+				
+
+				user.signUpInBackground(block: { (success, error) in
+					
+					self.activityIndicator.stopAnimating()
+					UIApplication.shared.endIgnoringInteractionEvents()
+					
+					if error != nil {
+						
+						var displayErrorMessage = "Please try again later"
+						
+						if let errorMessage = (error! as NSError).userInfo["error"] as? String {
+							
+							displayErrorMessage = errorMessage
+							
+						}
+						
+						self.createAlert(title: "Signup Error", message: displayErrorMessage)
+						
+					} else {
+						
+						print("user signed up")
+						
+						self.performSegue(withIdentifier: "showUserTable", sender: self)
+						
+					}
+					
+				})
+			
+			} else {
+				
+				// Login Mode
+				
+				PFUser.logInWithUsername(inBackground: emailTextField.text!, password: passwordTextField.text!, block: { (user, error) in
+					
+					self.activityIndicator.stopAnimating()
+					UIApplication.shared.endIgnoringInteractionEvents()
+					
+					if error != nil {
+						
+						var displayErrorMessage = "Please try again later"
+						
+						if let errorMessage = (error! as NSError).userInfo["error"] as? String {
+							
+							displayErrorMessage = errorMessage
+							
+						}
+						
+						self.createAlert(title: "Login Error", message: displayErrorMessage)
+						
+					} else {
+						
+						print("logged in ")
+						
+						self.performSegue(withIdentifier: "showUserTable", sender: self)
+						
+					}
+					
+					
+				})
+				
+				
+			}
 			
 		}
 		
@@ -69,6 +160,18 @@ class ViewController: UIViewController {
 			signupMode = true
 			
 		}
+		
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		
+		if PFUser.current() != nil {
+			
+			performSegue(withIdentifier: "showUserTable", sender: self)
+			
+		}
+		
+		self.navigationController?.navigationBar.isHidden = true
 		
 	}
 	
